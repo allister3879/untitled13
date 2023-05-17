b1165cf8747d57a7bf6dbd77165779bf0ea0de56
allister3879 <220956@astanait.edu.kz>
2023-05-17 15:53:11 +0600

    Node class

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,12 @@
+public class BinarySearchTree<K extends Comparable<K>, V> {
+    private Node root;
+    private class Node{
+        private K key;
+        private V value;
+        private Node left, right;
+        public Node(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
