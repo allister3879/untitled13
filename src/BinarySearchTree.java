8aa230e612831ddfcdcc25ae1057ef7f9579651b
allister3879 <220956@astanait.edu.kz>
2023-05-17 16:49:46 +0600

    'put' method

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,6 +1,6 @@
-public class BinarySearchTree<K extends Comparable<K>, V> {
+public class BinarySearchTree <K extends Comparable<K>, V> {
     private Node root;
-    private class Node{
+    private class Node {
         private K key;
         private V value;
         private Node left, right;
@@ -8,5 +8,32 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
             this.key = key;
             this.value = value;
         }
+    }
+    // methods
+    public void put(K key, V value){
+        if(isEmpty()){  //if the tree is empty we create a root node
+            root = new Node(key, value);
+        }else{
+            put(key, value, root);
+        }
+    }
+    private Node put (K key, V value, Node current){
+        if(key.compareTo(current.key) < 0){  //if key less than current key
+            if(current.left == null) {
+                current = new Node(key, value); // recurse to the left node until reach leaf node
+            }else {
+                current.left = put(key, value, current.left);
+            }
+        } else if (key.compareTo(current.key) > 0) { //if key greater than current key
+            if(current.right == null) {
+                current = new Node(key, value);  // recurse to the right node until reach leaf node
+            }else {
+                current.right = put(key, value, current.right);
+            }
+        }
+        return current;
+    }
+    public boolean isEmpty(){
+        return root==null;  // check if root empty
     }
 }
