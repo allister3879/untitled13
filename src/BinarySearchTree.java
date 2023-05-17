316fea62b1783329928da1e1a7f4daaa3c5319ec
allister3879 <220956@astanait.edu.kz>
2023-05-17 17:17:02 +0600

    'get' method

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -11,10 +11,18 @@ public class BinarySearchTree <K extends Comparable<K>, V> {
     }
     // methods
     public void put(K key, V value){
-        if(isEmpty()){  //if the tree is empty we create a root node
+        if(isEmpty()){  //if the tree is empty create a root node
             root = new Node(key, value);
         }else{
             put(key, value, root);
+        }
+    }
+    public V get(K key){
+        if(isEmpty()){ // if tree is empty return null
+            return null;
+        }else {
+            Node node = get(key, root);
+            return node.value;
         }
     }
     private Node put (K key, V value, Node current){
@@ -32,6 +40,18 @@ public class BinarySearchTree <K extends Comparable<K>, V> {
             }
         }
         return current;
+    }
+    private Node get(K key,Node current){
+        Node temp = new Node(null,null); //create a temp node to store the found node
+        if(key.equals(current.key)){
+            temp = current; // assign the current node to the temp node
+        }else {
+            if(key.compareTo(current.key)<0)
+                return get(key, current.left); // recursively search in the left subtree and return the result
+            if(key.compareTo(current.key)>0)
+                return get(key, current.right); //recursively search in the right subtree and return the result
+        }
+        return temp;  // return temp node
     }
     public boolean isEmpty(){
         return root==null;  // check if root empty
